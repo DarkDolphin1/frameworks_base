9596010a97845e9057b26aec7d091acc86954005
ezio84 <brabus84@gmail.com>
2019-11-02 17:46:14 +0100

    SystemUI: Allow to pulse on new tracks
    * @neobuddy89: Forward Port to Android 11/12/13/14/16
    
    neobuddy89:
    * Remove now playing package usage. Rely on metadata changes.
    * Improve doze pulse broadcast usage.
    * Add minor fixes and NPE guards.
    * Use BG executor for smooth UX.
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -83,6 +83,7 @@ public class AmbientDisplayConfiguration {
                 || pulseOnLongPressEnabled(user)
                 || pulseOnCustomDozeEventEnabled(user)
                 || alwaysOnEnabled(user)
+                || isAmbientTickerEnabled(user)
                 || wakeLockScreenGestureEnabled(user)
                 || wakeDisplayGestureEnabled(user)
                 || pickupGestureEnabled(user)
@@ -114,6 +115,11 @@ public class AmbientDisplayConfiguration {
     public boolean userPulseOnNotificationEnabled(int user) {
         return boolSettingDefaultOn(Settings.Secure.DOZE_FOR_NOTIFICATIONS, user)
                 && pulseOnNotificationEnabled(user);
+    }
+
+    /** @hide */
+    public boolean isAmbientTickerEnabled(int user) {
+        return boolSettingDefaultOff(Settings.Secure.PULSE_ON_NEW_TRACKS, user);
     }
 
     /** @hide */
