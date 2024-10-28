bdbfb199e8e5247901de0f18cf20100c450e6e7d
Tommy Webb <tommy@calyxinstitute.org>
2024-10-28 15:01:12 +0000

    fixup! Implement firewall status bar indicator
    Update the firewall indicator not only when transitioning to an app but
    also when the transition is completed. This matches the latest behavior
    of the work profile icon and resolves issues where it would not update.
    
    Issue: calyxos#2671
    Issue: calyxos#2672
    Change-Id: If53496ee624f43941649e25b598580a380f1aca8
    Signed-off-by: HDzungx <hdzungx@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -766,6 +766,7 @@ public class PhoneStatusBarPolicy
     public void appTransitionFinished(int displayId) {
         if (mDisplayId == displayId) {
             updateProfileIcon();
+            updateFirewall();
         }
     }
 
