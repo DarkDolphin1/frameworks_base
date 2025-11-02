1c8b8352b1edd8e0011682e07f4e28e9fd31041c
Pranav Vashi <neobuddy89@gmail.com>
2025-11-02 11:51:51 +0530

    ScrimUtils: Send dozing state change value to listeners
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/MediaViewController.kt
+++ packages/SystemUI/src/com/android/systemui/media/MediaViewController.kt
@@ -436,7 +436,7 @@ class MediaViewController @Inject constructor(
         }
     }
 
-    override fun onDozingChanged() {
+    override fun onDozingChanged(dozing: Boolean) {
         coroutineScope.launch {
             onMediaStateChanged()
         }
