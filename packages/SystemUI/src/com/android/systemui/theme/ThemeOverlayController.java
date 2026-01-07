273ebe662d5e9f27985866ce31f3fbe45ca0fc8c
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-01-08 06:32:54 +0800

    [theme_colors] updating color hooks impl
    Change-Id: I77c8f651f6776d77ce0f6bd6ced41c8d750bddbe
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -155,6 +155,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
     // UI contrast as reported by UiModeManager
     private double mContrast = 0.0;
     private double mChromaBoost = 0.0;
+    private boolean mIsFidelityEnabled = true;
     // Theme variant: Vibrant, Tonal, Expressive, etc
     @VisibleForTesting
     @Style.Type
@@ -634,6 +635,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         mMainWallpaperColor = mainColor;
 
         if (mIsMonetEnabled) {
+            fetchCustomThemeSettings();
             mThemeStyle = fetchThemeStyleFromSetting();
             createOverlays(mMainWallpaperColor);
             mNeedsOverlayCreation = true;
@@ -645,6 +647,28 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
 
         updateThemeOverlays();
     }
+
+    private void fetchCustomThemeSettings() {
+        final String overlayPackageJson = mSecureSettings.getStringForUser(
+                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
+                mUserTracker.getUserId());
+        if (!TextUtils.isEmpty(overlayPackageJson)) {
+            try {
+                JSONObject object = new JSONObject(overlayPackageJson);
+                mContrast = object.optDouble("_contrast_level", 0.0);
+                mChromaBoost = object.optDouble("_chroma_boost", 0.0);
+                mIsFidelityEnabled = object.optBoolean("_fidelity_enabled", true);
+                if (DEBUG) {
+                    Log.d(TAG, "Custom theme settings: contrast=" + mContrast
+                            + " chromaBoost=" + mChromaBoost
+                            + " fidelity=" + mIsFidelityEnabled);
+                }
+            } catch (JSONException e) {
+                Log.w(TAG, "Failed to parse custom theme settings.", e);
+            }
+        }
+    }
+
 
     /**
      * Return the main theme color from a given {@link WallpaperColors} instance.
@@ -675,8 +699,12 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
     }
 
     private void createOverlays(int color) {
-        mDarkColorScheme = new ColorScheme(color, true /* isDark */, mThemeStyle, mContrast);
-        mLightColorScheme = new ColorScheme(color, false /* isDark */, mThemeStyle, mContrast);
+        int style = mThemeStyle;
+        if (mIsFidelityEnabled) {
+            style = Style.CONTENT;
+        }
+        mDarkColorScheme = new ColorScheme(color, true /* isDark */, style, mContrast);
+        mLightColorScheme = new ColorScheme(color, false /* isDark */, style, mContrast);
         mColorScheme = isNightMode() ? mDarkColorScheme : mLightColorScheme;
 
         mAccentOverlay = newFabricatedOverlay("accent");
